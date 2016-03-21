e1a36c98e8d9974821a0264401e73e7d2c3d9590
STEPHEN FLYNN <stephenjflynn1@gmail.com>
2016-03-21 17:25:03 -0400

    Project can now be updated

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -23,6 +23,19 @@ class ProjectsController < ApplicationController
 	end
 
 
+	def edit 
+		@project = Project.find(params[:id])
+	end
+
+	def update
+		@project = Project.find(params[:id])
+		@project.update(project_params)
+
+		flash[:notice] = "Project has been updated."
+		redirect_to @project
+	end
+
+
 
     private 
 
